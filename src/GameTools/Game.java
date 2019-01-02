7888254470082b126a434b5bdfcee73238a600c4
maayan01 <45026947+maayan01@users.noreply.github.com>
2019-01-02 15:35:05 +0200

    Add files via upload

---
+++ src/GameTools/Game.java
@@ -0,0 +1,22 @@
+package GameTools;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Game
+{
+	//------------------fields-----------------------------
+		Set<Fruit> setFruits;
+		Set<Packman> setPackmans;
+	    
+		//------------------constructor------------------------
+		public Game ()
+		{
+			setFruits = new HashSet<Fruit>();
+			setPackmans = new HashSet<Packman>();
+		}
+		//------------------------------------------------------
+		
+		
+}

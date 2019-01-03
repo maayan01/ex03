decea9a3bf73e31dda531b139aec793e8fdd405c
maayan01 <45026947+maayan01@users.noreply.github.com>
2019-01-03 14:32:18 +0200

    Add files via upload

--- src/File_format/CSVread.java
+++ src/File_format/CSVread.java
@@ -13,6 +13,9 @@ import GIS.GIS_project;
 import GIS.My_GIS_element;
 import GIS.My_GIS_layer;
 import GIS.My_GIS_project;
+import GameTools.Fruit;
+import GameTools.Game;
+import GameTools.Packman;
 
 public class CSVread 
 {
@@ -24,7 +27,7 @@ public class CSVread
 		String cvsSplitBy = ",";
 		GIS_layer csvLayer= new My_GIS_layer();
 		int i=0;
-		
+
 		try 
 		{
 			br = new BufferedReader(new FileReader(csvFile));
@@ -45,7 +48,7 @@ public class CSVread
 		{
 			e.printStackTrace();
 		}
-        
+
 		finally 
 		{
 			if (br != null) 
@@ -61,5 +64,67 @@ public class CSVread
 			}
 		}
 		return csvLayer;
+	}
+	//----------------------------------------------------------------
+	public static Game CSVreaderGame(String path_file) 
+	{		
+		BufferedReader br = null;
+		String csvFile = path_file;
+		String line = "";
+		String cvsSplitBy = ",";
+		Game csvGame = new Game();
+		//GIS_layer csvLayer= new My_GIS_layer();
+		int i=0;
+
+		try 
+		{
+			br = new BufferedReader(new FileReader(csvFile));
+			GIS_project files= new My_GIS_project(); 
+			while ((line = br.readLine()) != null) 
+			{
+				System.out.println("line:"+i+" in the while loop ");
+				//The data start from the second line;
+				if(i>0) 
+				{
+					String[] gameData = line.split(cvsSplitBy);
+					//packman data
+					if(gameData[0].equals("P"))
+					{
+						System.out.println("packman read");
+						csvGame.add_p(new Packman(gameData) );
+					}
+					//fruit data
+					else if(gameData[0].equals("F"))
+					{
+						System.out.println("fruit read");
+						csvGame.add_f(new Fruit (gameData));
+					}
+				//	csvLayer.add(new My_GIS_element(gameData));
+				}
+				i++;
+			}
+		//	files.add(csvLayer);
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+
+		finally 
+		{
+			if (br != null) 
+			{
+				try 
+				{
+					br.close();
+				} 
+				catch (IOException e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+		//return csvLayer;
+		return csvGame;
 	}
 }
---
+++ src/File_format/TestCsvGame.java
@@ -0,0 +1,17 @@
+package File_format;
+
+import GIS.GIS_layer;
+import GameTools.Game;
+
+public class TestCsvGame 
+{
+ String path_file;
+
+ public static void main(String[] args) 
+	{
+		String path_file= "D:\\Users\\eli\\Documents\\ArielJAVAProjects\\Ex3\\csv_doc\\game_1543684662657.csv";
+		Game myGame = CSVread.CSVreaderGame(path_file) ;
+	    System.out.println(myGame);
+	}
+
+}

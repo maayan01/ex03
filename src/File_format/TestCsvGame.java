a8a78fcae613c2b69ad854d1d3ba2f6166cad3c4
maayan01 <45026947+maayan01@users.noreply.github.com>
2019-01-03 16:18:11 +0200

    Add files via upload

--- src/File_format/TestCsvGame.java
+++ src/File_format/TestCsvGame.java
@@ -5,13 +5,17 @@ import GameTools.Game;
 
 public class TestCsvGame 
 {
- String path_file;
+	String path_file;
 
- public static void main(String[] args) 
+	public static void main(String[] args) 
 	{
+		String fileName;
 		String path_file= "D:\\Users\\eli\\Documents\\ArielJAVAProjects\\Ex3\\csv_doc\\game_1543684662657.csv";
 		Game myGame = CSVread.CSVreaderGame(path_file) ;
-	    System.out.println(myGame);
+		System.out.println(myGame);
+
+		fileName ="Game01";
+		myGame.saveGame2csv(fileName);
 	}
 
 }
